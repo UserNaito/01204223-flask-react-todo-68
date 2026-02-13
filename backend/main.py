c72c92233c2fdfacfb28ff31afc25469cf316b42
Saran Chaikulseriwong <saranchaikulseriwong02@gmail.com>
2026-02-13 13:52:56 +0700

    Update TodoItem model and add migration script

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_migrate import Migrate   
+from sqlalchemy import Integer, String, ForeignKey                            # เพิ่ม import Foreignkey
+from sqlalchemy.orm import Mapped, mapped_column, relationship                # เพิ่ม import relatiohship
 
 app = Flask(__name__)
 CORS(app)
@@ -22,14 +24,35 @@ class TodoItem(db.Model):
     title: Mapped[str] = mapped_column(String(100))
     done: Mapped[bool] = mapped_column(default=False)
 
+    ##### เพิ่มส่วน relationship  ซึ่งตรงนี้จะไม่กระทบ schema database เลย (เพราะว่าไม่มีการ map ไปยังคอลัมน์ใดๆ)
+    comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
+
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
-            "done": self.done
+            "done": self.done,
+            "comments": [
+                comment.to_dict() for comment in self.comments
+            ]
         }
 
-"""
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    message: Mapped[str] = mapped_column(String(250))
+    ### สังเกตว่าในการประกาศด้านล่าง มีการระบุ ondelete ด้วย
+    todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id', ondelete="CASCADE"))
+
+    todo: Mapped["TodoItem"] = relationship(back_populates="comments")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            "todo_id": self.todo_id
+        }
+
+
 INITIAL_TODOS = [
     TodoItem(title='Learn Flask'),
     TodoItem(title='Build a Flask App'),
@@ -40,7 +63,7 @@ with app.app_context():
         for item in INITIAL_TODOS:
             db.session.add(item)
         db.session.commit()
-"""
+
 
 todo_list = [
     { "id": 1,
